eae04e2fec02d8560dd5de1299a42653aa4128f9
shikharani8 <55764075+shikharani8@users.noreply.github.com>
2022-10-24 01:48:13 +0100

    Update Config.js

--- react-project/src/Config.js
+++ react-project/src/Config.js
@@ -15,4 +15,4 @@ export const navItems = [
 
 export const menuUrl = 'http://dev.menu.ninja/api/menu/156?key=8j5vfe%24*pfb**rzt&pretty=1';
 
-export const serverUrl = 'http://ec2-34-194-172-50.compute-1.amazonaws.com:4000';
+export const serverUrl = 'http://localhost:4000';
